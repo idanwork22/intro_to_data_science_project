79998f22c4836ce2d0396ef71c36ae8e497819f2
Idan Chen <idanwork22@gmail.com>
2024-05-11 15:19:15 +0300

    add final csv

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from src.classes.manager_class import ManagerClass
 def main():
     manager = ManagerClass(snapshot_number=1)
 
-    # manager.extract_data_from_booking(booking_url='https://www.booking.com/searchresults.html?city=20088325',
+    manager.extract_data_from_booking(booking_url='https://www.booking.com/searchresults.html?city=20088325',
+                                      time_to_travel=30,
+                                      length_of_stay=5,
+                                      adults=2,
+                                      children=0,
+                                      room=1)
+
+    # manager.extract_data_from_expedia(expedia_url='https://www.expedia.com/Hotel-Search?destination=New%20York',
     #                                   time_to_travel=3,
     #                                   length_of_stay=5,
     #                                   adults=2,
     #                                   children=0,
     #                                   room=1)
-
-    manager.extract_data_from_expedia(expedia_url='https://www.expedia.com/Hotel-Search?destination=New%20York',
-                                      time_to_travel=3,
-                                      length_of_stay=5,
-                                      adults=2,
-                                      children=0,
-                                      room=1)
 
 if __name__ == '__main__':
     main()
