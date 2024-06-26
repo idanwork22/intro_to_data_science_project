3c29830a561d6139c339f6aac632babd32f11750
Idan Chen <idanwork22@gmail.com>
2024-06-26 15:54:38 +0300

    jupyter only

--- src/classes/singleton.py
+++
@@ -1,9 +0,0 @@
-class SingletonMeta(type):
-    _instances = {}  # Dictionary to store instance of each singleton class
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            print("Creating the instance")
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
